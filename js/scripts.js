49268ea2f8e84d936cc03976320f6e27ce58c047
CoreDiLeone <fireejuegos@gmail.com>
2025-03-15 01:35:27 -0300

    my first commit

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+const menuMobile = document.getElementById('navbar');
+const menuBtn = document.getElementById('btn-menu');
+const imageIcon = document.getElementById('btn-icon');
+const handlerMenu = ()=>{
+    menuMobile.classList.toggle('show');
+    if (imageIcon.src.includes('icon-hamburger.svg')) {
+        // Cambiar el ícono a icon-close.svg
+        imageIcon.src = '/images/icon-close.svg';
+    } else {
+        // Cambiar el ícono a icon-hamburger.svg
+        imageIcon.src = '/images/icon-hamburger.svg';
+    }
+}
+
+menuBtn.addEventListener('click', handlerMenu);
