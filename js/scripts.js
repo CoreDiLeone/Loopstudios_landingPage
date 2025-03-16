dd6c3a4caf5cebdce6ad0ca4ec9ebb4a2914b8de
CoreDiLeone <fireejuegos@gmail.com>
2025-03-16 01:54:41 -0300

    Styles have been added for desktop devices

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,8 @@
 const menuMobile = document.getElementById('navbar');
 const menuBtn = document.getElementById('btn-menu');
 const imageIcon = document.getElementById('btn-icon');
+const navbar = document.getElementById('scrolled-nav');
+
 const handlerMenu = ()=>{
     menuMobile.classList.toggle('show');
     if (imageIcon.src.includes('icon-hamburger.svg')) {
@@ -11,5 +13,38 @@ const handlerMenu = ()=>{
         imageIcon.src = 'images/icon-hamburger.svg';
     }
 }
-
 menuBtn.addEventListener('click', handlerMenu);
+
+/*Javascript for Scroll event */
+const heroImageOne = document.getElementById('img-mobile-scrolled');
+const heroImageTwo = document.getElementById('img-desktop-scrolled');
+
+const handlerScroll = (image) =>{
+
+    const scrollPosition = window.scrollY;
+    let imageHeight = image.getBoundingClientRect().height; 
+        if(scrollPosition > imageHeight){
+            navbar.classList.add('scrolled');
+        }else{
+            navbar.classList.remove('scrolled');
+        }
+}
+
+const onResizeOrScroll = () => {
+    const screenWidth = window.innerWidth; 
+
+
+    if (screenWidth <= 1200) {  
+        handlerScroll(heroImageOne);
+    } else {  
+        handlerScroll(heroImageTwo);
+    }
+};
+
+window.addEventListener('scroll', onResizeOrScroll);
+window.addEventListener('resize', onResizeOrScroll);
+document.addEventListener('DOMContentLoaded', onResizeOrScroll);
+
+
+
+
